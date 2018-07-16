b545c46aa886ab3c134f4f197f5fe8064d9261c4
sebbeth <developer2025@gmail.com>
2018-07-16 23:20:05 +1000

    asdf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent }      from './landing/landing.component';
+import {ConceptComponent } from './concept/concept.component';
 
 const routes: Routes = [
 
   { path: '', component: LandingComponent },
+  { path: 'concept', component ConceptComponent}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,15 @@ import { ChartsModule } from 'ng2-charts';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { LandingComponent } from './landing/landing.component';
-import { NavigationComponent } from './navigation/navigation.component'; // From https://github.com/bootsoon/ng-circle-progress
+import { NavigationComponent } from './navigation/navigation.component';
+import { ConceptComponent } from './concept/concept.component'; // From https://github.com/bootsoon/ng-circle-progress
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    NavigationComponent
+    NavigationComponent,
+    ConceptComponent
   ],
   imports: [
     BrowserModule,
